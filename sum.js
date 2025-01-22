573abbdafc1d12251e79da70ccf3bc18cc9cd4fe
Dev Sukumar <54317674+vevnkataSukumar@users.noreply.github.com>
2025-01-22 19:53:15 +0530

    Calling Add with a negative number will throw an exception “negatives not allowed” - and the negative that was passed and clearing unnecessary  charecters other than number, comma and -

--- sum.js
+++ sum.js
@@ -9,10 +9,16 @@ export function sum(stringWithNumbers) {
 
     const cleanString = clearString(stringWithNumbers);
 
-    const numbers = cleanString.split(',')?.filter((num) => num && num < 1000);
-    return numbers?.reduce((acc, num) => acc + parseInt(num), 0);
-  }
+    const numberString = cleanString.split(',');
+    const numbersArray = getNumbersArray(numberString);
 
+    if (doesNumbersArrayHaveNegativeNumber(numbersArray)) {
+        return 'negatives not allowed';
+    }
+
+    const sumResult = sumNumbers(numbersArray);
+    return sumResult;
+  }
 
   function isString(val) {
     return typeof val === 'string';
@@ -20,8 +26,21 @@ export function sum(stringWithNumbers) {
 
   function clearString(stringWithNumbers) {
     // Match groups of consecutive digits
-    const numberGroups = stringWithNumbers.match(/\d+/g);
+    const numberGroups = stringWithNumbers.match(/-?\d+/g);
     
     // Join the number groups with commas
     return numberGroups ? numberGroups.join(',') : '';
+  }
+
+  function getNumbersArray(numString) {
+    return  numString?.filter((num) => num && num < 1000);
+  }
+
+  function sumNumbers(numArray) {
+    return numArray?.reduce((acc, num) => acc + parseInt(num), 0);
+  }
+
+  function doesNumbersArrayHaveNegativeNumber(numbersArray) {
+    // Check if any number in the array is negative
+    return numbersArray.some(num => num < 0);
   }
--- sum.test.js
+++ sum.test.js
@@ -31,6 +31,14 @@ describe('Sum Function', () => {
     test('add numbers in the string "abc123!@#,45de*67?890" and retun result as 123 and ignoring 1000', () => {
         expect(sum("abc123!@#,45de*67?890")).toBe(1125);
     });
+
+    test('add numbers in string [*][%]\n1*2%3 and retun result as 3', () => {
+        expect(sum('[*][%]\n1*2%3')).toBe(6);
+    });
+
+    test('add numbers in string $$$1,23%%%,[[__))-3] and retun result as negatives not allowed', () => {
+        expect(sum('$$$1,23%%%,[[__))-3]')).toBe('negatives not allowed');
+    });
 });
 
 
