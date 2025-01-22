9b879ed75ac2c031da782b5d7484e2fabcb6afe3
Dev Sukumar <54317674+vevnkataSukumar@users.noreply.github.com>
2025-01-22 20:04:24 +0530

    Code commenting

--- sum.js
+++ sum.js
@@ -1,29 +1,39 @@
 export function sum(stringWithNumbers) {
-    if (stringWithNumbers === '') {
-      return 0;
-    }
-
+    // Check if the input is a string
     if (!isString(stringWithNumbers)) {
         return undefined;
     }
 
+    // Check if the string is empty
+    if (stringWithNumbers === '') {
+      return 0;
+    }
+
+    // Remove all characters that are not numbers, commas and -
     const cleanString = clearString(stringWithNumbers);
 
+    // Split the string into an array of numbers
     const numberString = cleanString.split(',');
+
+    // Filter out numbers greater than 1000
     const numbersArray = getNumbersArray(numberString);
 
+    // Check if the array has any negative numbers
     if (doesNumbersArrayHaveNegativeNumber(numbersArray)) {
         return 'negatives not allowed';
     }
 
+    // Sum the numbers in the array
     const sumResult = sumNumbers(numbersArray);
     return sumResult;
   }
 
+  // function for checking if the input is a string
   function isString(val) {
     return typeof val === 'string';
   }
 
+  // function for removing all characters that are not numbers, commas and -
   function clearString(stringWithNumbers) {
     // Match groups of consecutive digits
     const numberGroups = stringWithNumbers.match(/-?\d+/g);
@@ -32,15 +42,18 @@ export function sum(stringWithNumbers) {
     return numberGroups ? numberGroups.join(',') : '';
   }
 
+  // fuction for converting string to array of numbers aqnd filtering out the numbers which are greater than 1000
   function getNumbersArray(numString) {
     return  numString?.filter((num) => num && num < 1000);
   }
 
-  function sumNumbers(numArray) {
-    return numArray?.reduce((acc, num) => acc + parseInt(num), 0);
-  }
-
+  // function for checking if the array has any negative numbers
   function doesNumbersArrayHaveNegativeNumber(numbersArray) {
     // Check if any number in the array is negative
     return numbersArray.some(num => num < 0);
+  }
+
+  // function for summing the numbers in the array
+  function sumNumbers(numArray) {
+    return numArray?.reduce((acc, num) => acc + parseInt(num), 0);
   }
--- sum.test.js
+++ sum.test.js
@@ -1,41 +1,52 @@
 import {sum} from './sum';
 
 describe('Sum Function', () => {
+    // test case for undefined scenario
     test('should return undefined if value is not astring', () => {
         expect(sum(1)).toBe(undefined);
     });
+
+    // test case for empty string and returning 0
     test('add numbers in string and retun result', () => {
         expect(sum('')).toBe(0);
     });
 
+    // test case for string with 1 number in it and returning the number
     test('add numbers in string with 1 number in it should retun as 1', () => {
         expect(sum('1')).toBe(1);
     });
 
+    // test case for string with 2 numbers in it and returning the sum of the numbers
     test('add numbers in string with numbers 2,5 and retun result as 7', () => {
         expect(sum('2,5')).toBe(7);
     });
 
+    // test case for string with multiple numbers in it and returning the sum of the numbers
     test('add numbers in string with numbers 2,5,6,7,8,10 and retun result as 38', () => {
         expect(sum('2,5,6,7,8,10')).toBe(38);
     });
 
+    // test case for string with multiple numbers in it and returning the sum of the numbers and ignoring numbers greater than 1000
     test('add numbers in string with numbers 2,5,6,7,8,10001 and retun result as 28 and ignoring 10001', () => {
         expect(sum('2,5,6,7,8,10001')).toBe(28);
     });
 
+    // test case for string with numbers in it and removing special charecters other than numbers, comma and - from it and returning the sum of the numbers
     test('add numbers in string ;\n1;2 and retun result as 3', () => {
         expect(sum(';\n1;2')).toBe(3);
     });
 
+    // test case for string with numbers in it and removing special charecters other than numbers, comma and - from it and returning the sum of the numbers
     test('add numbers in the string "abc123!@#,45de*67?890" and retun result as 123 and ignoring 1000', () => {
         expect(sum("abc123!@#,45de*67?890")).toBe(1125);
     });
 
+    // test case for string with numbers in it and removing special charecters other than numbers, comma and - from it and returning the sum of the numbers
     test('add numbers in string [*][%]\n1*2%3 and retun result as 3', () => {
         expect(sum('[*][%]\n1*2%3')).toBe(6);
     });
 
+    // test case for string with negative numbers in it and removing special charecters other than numbers, comma and - from it and returning the sum of the numbers
     test('add numbers in string $$$1,23%%%,[[__))-3] and retun result as negatives not allowed', () => {
         expect(sum('$$$1,23%%%,[[__))-3]')).toBe('negatives not allowed');
     });
